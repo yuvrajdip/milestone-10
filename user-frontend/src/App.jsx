6f4fb6aac501b720cf81a546df4dfddf935d3bc7
yuvrajdip <dipbhowmik2018@gmail.com>
2023-10-17 21:10:12 +0600

    Module 54 fully done

--- user-frontend/src/App.jsx
+++ user-frontend/src/App.jsx
@@ -10,10 +10,44 @@ function App() {
     .then(data=> setUsers(data))
   },[])
 
+  const handleAddUser=(e)=>{
+    e.preventDefault();
+    const form = e.target
+    const name = form.name.value 
+    const email = form.email.value 
+    const user = {name , email };
+    console.log(user);
+
+    //* think it as get api
+    fetch('http://localhost:5001/users',{
+          method:'POST',
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify(user)
+        }  
+      )
+    .then( res=> res.json())
+    .then( data=> {
+      console.log(data)
+      const newUsers = [...users, data];
+      setUsers( newUsers)
+    })
+  }
+
   return (
     <>
-      <h1>Users frontend</h1>
-      <h2>Users length : {users.length}</h2>
+      <form action="" onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <input type="submit" value="submit" />
+      </form>
+      <p>Users frontend</p>
+      <p>Users length : {users.length}</p>
+      {
+        users.map( user => <p key={user.id}>{`${user.id}. ${user.email}`}</p>)
+      }
     </>
   )
 }
--- users-server/index.js
+++ users-server/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const port = process.env.PORT || 5001 ;
+
+//* middleware
+app.use(cors())
+app.use(express.json())
 
 const users = [
   {id:1 , name : "Sabina", email : "sabina@gmail.com"},
@@ -14,6 +19,18 @@ app.get('/',(req,res)=>{
 
 app.get('/users',(req,res)=>{
   res.send(users)
+})
+
+//* create a post api on the server side
+app.post('/users', (req,res)=>{
+  console.log('post api hitting')
+  console.log(req.body)
+
+  const newUser = req.body 
+  newUser.id = users.length +1 
+  users.push( newUser)
+  
+  res.send( newUser )
 })
 
 app.listen(port, ()=> {
